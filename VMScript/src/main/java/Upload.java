936609504c4400bedef0b788c1b35d9fdd9b5dd6
Tyler Wong <ty-w@live.com>
2016-11-04 01:07:14 -0700

    Fix compiler errors

--- VMScript/src/main/java/Upload.java
+++ VMScript/src/main/java/Upload.java
@@ -1,8 +1,21 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 public class Upload {
-	FirebaseOptions options = new FirebaseOptions.Builder()
-	  .setServiceAccount(new FileInputStream("path/to/serviceAccountCredentials.json"))
-	  .setDatabaseUrl("https://databaseName.firebaseio.com/")
-	  .build();
-	FirebaseApp.initializeApp(options);
+	public void firebase() {
+		FirebaseOptions options;
+		try {
+			options = new FirebaseOptions.Builder()
+			  .setServiceAccount(new FileInputStream("path/to/serviceAccountCredentials.json"))
+			  .setDatabaseUrl("https://databaseName.firebaseio.com/")
+			  .build();
+			FirebaseApp.initializeApp(options);
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }

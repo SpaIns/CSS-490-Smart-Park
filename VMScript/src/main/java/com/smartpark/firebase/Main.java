b7f98311044ffce67971b39430083da64e0551ae
Tyler Wong <ty-w@live.com>
2016-11-06 11:49:31 -0800

    Move constants to variables

--- VMScript/src/main/java/com/smartpark/firebase/Main.java
+++ VMScript/src/main/java/com/smartpark/firebase/Main.java
@@ -1,8 +1,12 @@
 package com.smartpark.firebase;
 
 public class Main {
+	
+	private final static String datebaseUrl = "https://smartpark-aa8eb.firebaseio.com/";
+	private final static String serviceAccountPath = "C:/Users/Tyler/Desktop/SmartPark-a58d3074b302.json";
+	
 	public static void main(String[] args) {
-		Firebase fb = new Firebase("https://smartpark-aa8eb.firebaseio.com/", "C:/Users/Tyler/Desktop/SmartPark-a58d3074b302.json");
+		Firebase fb = new Firebase(datebaseUrl, serviceAccountPath);
 		fb.uploadData(fb.getData());
 		System.exit(0);
 	}

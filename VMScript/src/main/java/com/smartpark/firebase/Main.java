5f539fa92c34460c8dc56f8bf583f6c899de93ba
Tyler Wong <ty-w@live.com>
2016-11-09 00:03:37 -0800

    Use full service account path and update compiled jar

--- VMScript/src/main/java/com/smartpark/firebase/Main.java
+++ VMScript/src/main/java/com/smartpark/firebase/Main.java
@@ -3,7 +3,7 @@ package com.smartpark.firebase;
 public class Main {
 	
 	private final static String datebaseUrl = "https://smartpark-aa8eb.firebaseio.com/";
-	private final static String serviceAccountPath = "./SmartPark-a58d3074b302.json";
+	private final static String serviceAccountPath = "/home/ubuntu/VMScripts/SmartPark-a58d3074b302.json";
 	private final static String spacesFilePath = "allSpaces.txt";
 	
 	public static void main(String[] args) {		

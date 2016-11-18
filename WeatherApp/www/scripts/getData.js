12cdd605e440a9bba846a2d5ec27837d45854af1
eliasmuche <mucheelias@yahoo.com>
2016-11-17 18:51:32 -0800

    Changed back to acessing the flat list
    The data is still accurate

--- WeatherApp/www/scripts/getData.js
+++ WeatherApp/www/scripts/getData.js
@@ -1,82 +1,7 @@
 // JavaScript source code
 //General Script to show parking data
 
-/*
-function obtainData(garage,floor) {
-    var config = {
-        apiKey: "AIzaSyDIkSj_PO9zfpbqYcoeDfton9NLPkQUdrI",
-        databaseURL: "https://smartpark-aa8eb.firebaseio.com",
-    };
-    firebase.initializeApp(config);
 
-    var database = firebase.database(); //get a database reference
-    link = '/spaces/';
-    begin = 0;
-    end = 0;
-    if (garage=='NorthGarage') {
-
-        if (floor=='Floor1') {
-            begin = 2001;
-            end = 2475;
-        }
-        else if(floor=='Floor2'){
-            begin=2117;
-            end = 2228;
-        }
-        else if(floor=='Floor3'){
-            begin= 2229;
-            end = 2454;
-        }
-        else{
-            begin = 2336;
-            end = 2453;
-        }
-    }
-    else{
-        if (floor == 'Floor1') {
-            begin = 126;
-            end = 297;
-        }
-        else if (floor == 'Floor2') {
-            begin = 298;
-            end = 457;
-        }
-        else if (floor == 'Floor3') {
-            begin = 460;
-            end = 627;
-        }
-        else if (floor == 'Floor4') {
-            begin= 628;
-            end= 791;
-        }
-        else {
-            begin= 792;
-            end = 907;
-        } 
-    }
-    
-    for (i = begin; i <= end; i++) {
-
-
-        var spaceNumer = null;
-        var spaceType = null;
-        var avail = null;
-        
-        firebase.database().ref(link + i).once('value').then(function (snapshot) {
-            setTimeout(function () {
-                spaceNumber = snapshot.val().spaceNumber;
-                spaceType = snapshot.val().spaceType;
-                avail = snapshot.val().isAvailable;
-                document.write("Space: "+spaceNumber + ", ");
-                document.write("Type: "+spaceType + ",  ");
-                document.write("Open: "+avail + "<br>"+"<br>");
-            },4000);
-            
-
-       });
-    } 
-}
-*/
 function obtainData(garage, floor) {
     var config = {
         apiKey: "AIzaSyDIkSj_PO9zfpbqYcoeDfton9NLPkQUdrI",
@@ -85,30 +10,15 @@ function obtainData(garage, floor) {
     firebase.initializeApp(config);
 
     var database = firebase.database(); //get a database reference
-    link = "/parking/" + garage +"/" +floor;
-    x = [];
-    
+    link = "/spaces";
       firebase.database().ref(link).once('value').then(function (snap) {
-         setTimeout(function () {
-           
              snap.forEach(function (cSnap) {
-                 setTimeout(function () {
-                     val=cSnap.val()
-                     if(val.isAvailable==true){
-                         document.write("Space: " + val.spaceNumber + " Type: " + val.spaceType+"<br>");
-                     }
-                     //Next three lines are for printing all spaces instead of available ones
-                     //document.write("Space: "+val.spaceNumber);
-                     //document.write(" Type: "+val.spaceType);
-                     //document.write(" Open: "+val.isAvailable+"<br>");
-
-                 },100);
+                val = cSnap.val();
+                if (val.garage == garage && val.floor== floor) {
+                   document.writeln(val.spaceNumber +"<br>");     
+                }
              });
 
-          }, 4000);
-
       });
-
-    
 }
 

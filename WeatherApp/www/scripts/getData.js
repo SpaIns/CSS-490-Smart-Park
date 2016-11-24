916bd9dbe121f29e05b755ec5c0b956069023252
eliasmuche <mucheelias@yahoo.com>
2016-11-23 17:04:03 -0800

    Storing Data into locally works now
    This means that getData.js can communicate with other components of the
    project. This should help with integration.

--- WeatherApp/www/scripts/getData.js
+++ WeatherApp/www/scripts/getData.js
@@ -1,5 +1,6 @@
 // JavaScript source code
 //General Script to show parking data
+var data = [];
 
 
 function obtainData(garage, floor) {
@@ -11,14 +12,38 @@ function obtainData(garage, floor) {
 
     var database = firebase.database(); //get a database reference
     link = "/spaces";
-      firebase.database().ref(link).once('value').then(function (snap) {
-             snap.forEach(function (cSnap) {
+    return new Promise(function (resolve, reject) {
+        firebase.database().ref(link).once('value').then(function (snap) {
+            snap.forEach(function (cSnap) {
                 val = cSnap.val();
-                if (val.garage == garage && val.floor== floor) {
-                   document.writeln(val.spaceNumber +"<br>");     
-                }
-             });
+                if (val.garage == garage && val.floor == floor && val.isAvailable == "true") {
+                    //document.writeln(val.spaceNumber +"<br>");
+                    dict = { "spaceNumber": val.spaceNumber, "spaceType": val.spaceType };
+                    data.push(dict);
+                    //showArray();
 
-      });
+
+                }
+            });
+            resolve(data);
+        });
+
+    });
+    
+
+
+}
+
+
+function getArray(fireData) {
+    return data;
+}
+
+function showArray() {
+    document.writeln(data.length);
+    //for (i = 0; i < data.length; i++) {
+      //  document.writeln(data[i]["spaceNumber"]);
+    //}
+
 }
 

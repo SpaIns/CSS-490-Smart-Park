ba9775a70bdff7097c9e10c67784f3e4bc384c33
Spalns <sachtmann121@gmail.com>
2016-12-06 19:00:04 -0800

    Fixed favorite lot for real

---
+++ WeatherApp/www/scripts/favoriteLotExecution.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#favorite-lot').click(function () {
+        alert("inside the function");
+        var storage = window.localStorage;
+        var href = storage.getItem("favoriteLot");
+        if (href != null) {
+            //now redirect
+            window.location.replace(href);
+        }
+        else {
+            //favorite lot not yet set
+            alert("No favorite lot set - please set in the preferences page");
+        }
+    });
+});
